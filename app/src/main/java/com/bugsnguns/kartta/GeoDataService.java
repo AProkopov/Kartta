278b8f936c3f03ab8993e98f71bbfdb6061370b5
LongLakesIPT <prokopov.anton@gmail.com>
2016-11-10 20:12:02 +0300

    added a service and startGeoTracking method (startButton)

---
+++ app/src/main/java/com/bugsnguns/kartta/GeoDataService.java
@@ -0,0 +1,30 @@
+package com.bugsnguns.kartta;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class GeoDataService extends Service {
+    public GeoDataService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public static void trackRecorder() {
+        //this for-loop just for test
+        for (int i = 0; i <=10; i++) {
+            Log.v("TEST trackRecorder", "trackRecorder is running " + i);
+        }
+
+    }
+}
--- app/src/main/java/com/bugsnguns/kartta/MapsActivity.java
+++ app/src/main/java/com/bugsnguns/kartta/MapsActivity.java
@@ -1,8 +1,13 @@
 package com.bugsnguns.kartta;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,9 +15,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    public ArrayList<LatLng> geoLocationList = new ArrayList<>();
+
 
 
     @Override
@@ -24,6 +33,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //start GeoDataService
+        Intent serviceIntent = new Intent(this, GeoDataService.class);
+        startService(serviceIntent);
     }
 
     @Override
@@ -32,15 +44,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         return true;
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -50,5 +53,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    public void startGeoTracking(View view) {
+        Log.v("START_BUTTON onClick", "startButton clicked");
+        GeoDataService.trackRecorder();
+    }
+
+
+
 
 }
