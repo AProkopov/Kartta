070b2bb434d8f94e4085b23cc92ec17856b98ef2
LongLakesIPT <prokopov.anton@gmail.com>
2017-05-16 23:55:23 +0300

    TrackActivity added

---
+++ app/src/main/java/com/bugsnguns/kartta/TrackActivity.java
@@ -0,0 +1,14 @@
+package com.bugsnguns.kartta;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TrackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track);
+    }
+}
